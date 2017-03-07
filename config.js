c25e062fdf5d441839c82c1b4237231d007e70b5
Jesse L.K. Overton <jesse@ovrtn.com>
2017-03-07 13:53:08 -0500

    Update for docker build

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 
   // Your bot name. Typically, this is your bot's username without the discriminator.
   // i.e: if your bot's username is MemeBot#0420, then this option would be MemeBot.
-  name: "",
+  name: process.env.BOT_NAME,
 
   // The bot's command prefix. The bot will recognize as command any message that begins with it.
   // i.e: "-asheron foo" will trigger the command "foo",
@@ -12,10 +12,17 @@ module.exports = {
   // Your bot's user token. If you don't know what that is, go here:
   // https://discordapp.com/developers/applications/me
   // Then create a new application and grab your token.
-  token: "",
+  token: process.env.BOT_USER_TOKEN,
 
   webhooks: {
+      reddit: {
+          id: process.env.REDDIT_WEBHOOK_ID,
+          token: process.env.REDDIT_WEBHOOK_TOKEN
+      }
   },
+
+  apiKeyTemp: process.env.API_SECRET_TEMP,
+
 
   // If this option is enabled, the bot will delete the message that triggered it, and its own
   // response, after the specified amount of time has passed.
